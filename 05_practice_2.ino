1e555b32171c1d6afe46475cf162174e90a74958
ParkSeungHwan01 <128887013+ParkSeungHwan01@users.noreply.github.com>
2023-09-24 15:33:59 +0900

    05_practice_2.ino

--- 05_practice_2.ino
+++ 05_practice_2.ino
@@ -5,8 +5,8 @@ void setup() {
 }
 
 void loop() {
-    digitalWrite(PIN_LED, 0); // LED를 켭니다
-    delay(1000); // 1초 동안 대기합니다.
+    digitalWrite(PIN_LED, 0); 
+    delay(1000); 
 
     digitalWrite(PIN_LED, 1);
     delay(100);
